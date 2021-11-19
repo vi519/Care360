f3e58f229c464458d0e9d5b9de9398810a1d1876
vi519 <68097369+vi519@users.noreply.github.com>
2021-11-19 17:18:37 +0530

    vineet added

---
+++ src/AdultMeasures.svelte
@@ -0,0 +1,48 @@
+<script>
+      
+   
+</script>
+
+<main >
+
+    <div class="  dark:bg-gray-800" />
+    <div class=" bg-blue-500 rounded overflow-hidden shadow-lg">
+      <!-- <img class="w-full" src="/img/card-top.jpg" alt="Sunset in the mountains"> -->
+      <div class="  text-center font-bold text-xl mb-4">Colorectal Screening (COL-E)
+
+    
+      </div>
+      <div class="float-right px-2 " > Hide Details</div>
+     <div>
+
+
+     </div>
+     
+    </div>
+    <div class=" bg-pink-400  content-center  container w-11/12  ml-6">
+        <div class="rounded overflow-hidden shadow-lg  px-4 "> 
+        <div class="text-lg mt-4 mb-2">
+            ✔	Patient Age 75 <br/>
+            ❌ Last Colonscopy done 10/09/2001 <br/>
+            ❌ No Simoidosocpy records <br/>
+            ❌ No Cologuard  records <br/>
+            ❌ No Fecal Occult Blood Test <br/>
+
+        </div>
+
+
+        <div class="text-lg font-bold mt-2 "> Recomendations : </div>
+        <div class="mb-2">
+            Order Colonscopy <br>
+            Order Sigmoidoscopy <br>
+            Order Cologuard <br>
+            Order Occult Blood Test
+    
+        <button class="bg-green-500 float-right px-4">Overide</button>
+    
+          </div>
+
+</main>
+
+<style>
+</style>
