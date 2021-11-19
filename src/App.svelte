4247e89c8072cc9ace656b38cb6a5521a7fd5b55
Mezeru <jermpro0@gmail.com>
2021-11-19 14:07:28 +0530

    First Commit

---
+++ src/App.svelte
@@ -0,0 +1,5 @@
+<script lang="ts">
+  import Comp from "./components/Care360.svelte";
+</script>
+
+<Comp />
---
+++ src/components/Care360.svelte
@@ -0,0 +1,26 @@
+<script lang="ts">
+  let a: Number = null;
+</script>
+
+<sl-tab-group>
+  <sl-tab slot="nav" panel="Add">Add Measures</sl-tab>
+  <sl-tab slot="nav" panel="Women">Women's Measures</sl-tab>
+  <sl-tab slot="nav" panel="Medications">Medications</sl-tab>
+  <sl-tab slot="nav" panel="Diabetic">Diabetic Care</sl-tab>
+
+  <sl-tab-panel name="Add">
+    <div>Add Measures</div>
+  </sl-tab-panel>
+
+  <sl-tab-panel name="Women">
+    <div>Women's Measures</div>
+  </sl-tab-panel>
+
+  <sl-tab-panel name="Medications">
+    <div>Medications</div>
+  </sl-tab-panel>
+
+  <sl-tab-panel name="Diabetic">
+    <div>Diabetic Care</div>
+  </sl-tab-panel>
+</sl-tab-group>
